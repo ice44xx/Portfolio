b7205dd3e8f550231fe7863adbb197f746709a4a
Nathan Sant Helena <ice44xx@gmail.com>
2024-01-12 03:46:49 -0300

    feat: inserting useMemo in navbar and globe

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import styles from './style.module.scss';
 import CloseBtn from '@/common/close';
 
@@ -20,40 +20,44 @@ const Navbar: React.FC<Props> = ({ changePage }) => {
     setActive(!active);
   };
 
-  return (
-    <>
-      <CloseBtn click={handleToggle} active={active} />
-      <div className={`${styles.navbar} ${active ? styles.navbar : styles.navbarHidden}`}>
-        <ul>
-          <li>
-            <button className={`${styles.btn} ${styles.btn1}`} onClick={() => changeActiveSection('home')}>
-              Home
-            </button>
-          </li>
-          <li>
-            <button className={`${styles.btn} ${styles.btn2}`} onClick={() => changeActiveSection('sobre')}>
-              Sobre
-            </button>
-          </li>
-          <li>
-            <button className={`${styles.btn} ${styles.btn3}`} onClick={() => changeActiveSection('habilidades')}>
-              Habilidades
-            </button>
-          </li>
-          <li>
-            <button className={`${styles.btn} ${styles.btn4}`} onClick={() => changeActiveSection('projetos')}>
-              Projetos
-            </button>
-          </li>
-          <li>
-            <button className={`${styles.btn} ${styles.btn5}`} onClick={() => changeActiveSection('contato')}>
-              Redes
-            </button>
-          </li>
-        </ul>
-      </div>
-    </>
-  );
+  const memoizedNavbar = useMemo(() => {
+    return (
+      <>
+        <CloseBtn click={handleToggle} active={active} />
+        <div className={`${styles.navbar} ${active ? styles.navbar : styles.navbarHidden}`}>
+          <ul>
+            <li>
+              <button className={`${styles.btn} ${styles.btn1}`} onClick={() => changeActiveSection('home')}>
+                Home
+              </button>
+            </li>
+            <li>
+              <button className={`${styles.btn} ${styles.btn2}`} onClick={() => changeActiveSection('sobre')}>
+                Sobre
+              </button>
+            </li>
+            <li>
+              <button className={`${styles.btn} ${styles.btn3}`} onClick={() => changeActiveSection('habilidades')}>
+                Habilidades
+              </button>
+            </li>
+            <li>
+              <button className={`${styles.btn} ${styles.btn4}`} onClick={() => changeActiveSection('projetos')}>
+                Projetos
+              </button>
+            </li>
+            <li>
+              <button className={`${styles.btn} ${styles.btn5}`} onClick={() => changeActiveSection('contato')}>
+                Redes
+              </button>
+            </li>
+          </ul>
+        </div>
+      </>
+    );
+  }, [active, activeSection]);
+
+  return memoizedNavbar;
 };
 
 export default Navbar;
--- src/components/skills/globe.tsx
+++ src/components/skills/globe.tsx
@@ -1,133 +1,57 @@
-import React from 'react';
-import { Cloud, ICloud, renderSimpleIcon } from 'react-icon-cloud';
-import {
-  siJavascript,
-  siNextdotjs,
-  siReact,
-  siPhp,
-  siLaravel,
-  siCss3,
-  siHtml5,
-  siSass,
-  siNginx,
-  siBulma,
-  siNodemon,
-  siDocker,
-  siGit,
-  siGithub,
-  siTypescript,
-  siWindows,
-  siLinux,
-  siFigma,
-  siAdobephotoshop,
-  siPostgresql,
-  siMysql,
-  siMicrosoftsqlserver,
-  siMongodb,
-  siXampp
-} from 'simple-icons/icons';
+import React, { useMemo } from 'react';
+import { Cloud, ICloud, renderSimpleIcon, SimpleIcon } from 'react-icon-cloud';
+import * as Icons from 'simple-icons/icons';
 
 const options: ICloud['options'] = {
   clickToFront: 500,
   imageScale: 1.2,
   initial: [0.1, -0.01],
-  // interval: number
-  // lock: null | 'x' | 'y' | 'xy'
-  // maxBrightness: number
-  // minBrightness: number
-  // minSpeed: number
-  // minTags: 0 - 200
   noMouse: true,
-  // noSelect: boolean
-  // noTagsMessage: string
-  // offsetX: number
-  // offsetY: number
   outlineColour: '#0000',
-  // outlineDash: number
-  // outlineDashSpace: number
-  // outlineIncrease: number
-  // outlineMethod: 'outline' | 'classic' | 'block' | 'colour' | 'size' | 'none'
-  // outlineOffset: number
-  // outlineRadius: number
-  // outlineThickness: number
-  // padding: number
-  // pinchZoom: boolean
-  // pulsateTime: number
-  // pulstateTo: number
-  // radiusX: number
-  // radiusY: number
-  // radiusZ: number
-  // repeatTagsTags: 0 - 64
   reverse: true,
-  // scrollPause: boolean
-  // shadow: string
-  // shadowBlur: number
-  // shadowOffset: [number,number] | number[]
-  // shape: 'sphere' | 'hcylinder' | 'vcylinder' | 'hring' | 'vring'
-  // shuffleTags: boolean
-  // splitWidth: number
-  // stretchX: number
-  // stretchY: number
-  // textAlign: 'centre' | 'left' | 'right'
-  // textColour: string
-  // textFont: string
-  // textHeight: number
-  // textVAlign: 'top' | 'bottom' | 'middle'
-  tooltip: 'native', // null | 'div'
-  // tooltipClass: string
+  tooltip: 'native',
   tooltipDelay: 0,
-  // txtOpt: boolean
-  // txtScale: number
-  // weight: boolean
-  // weightFrom: any
-  // weightGradient: any
-  // weightMode: 'size' | 'colour' | 'both' | 'bgcolour' | 'bgoutline' | 'outline'
-  // weightSize: number
-  // weightSizeMax: number | null
-  // weightSizeMin: number | null
   wheelZoom: false
-  // zoom: number
-  // zoomMax: number
-  // zoomMin: number
-  // zoomStep: number
 };
-const icons = [
-  siJavascript,
-  siNextdotjs,
-  siReact,
-  siCss3,
-  siHtml5,
-  siSass,
-  siNginx,
-  siBulma,
-  siNodemon,
-  siDocker,
-  siGit,
-  siGithub,
-  siTypescript,
-  siPhp,
-  siLaravel,
-  siWindows,
-  siLinux,
-  siFigma,
-  siAdobephotoshop,
-  siPostgresql,
-  siMysql,
-  siMicrosoftsqlserver,
-  siMongodb,
-  siXampp
-].map(icon => {
-  return renderSimpleIcon({
-    icon,
-    size: 75,
-    aProps: {
-      onClick: (e: any) => e.preventDefault()
-    }
-  });
-});
+
+const iconsList: SimpleIcon[] = [
+  Icons.siJavascript,
+  Icons.siNextdotjs,
+  Icons.siReact,
+  Icons.siCss3,
+  Icons.siHtml5,
+  Icons.siSass,
+  Icons.siNginx,
+  Icons.siBulma,
+  Icons.siNodemon,
+  Icons.siDocker,
+  Icons.siGit,
+  Icons.siGithub,
+  Icons.siTypescript,
+  Icons.siPhp,
+  Icons.siLaravel,
+  Icons.siWindows,
+  Icons.siLinux,
+  Icons.siFigma,
+  Icons.siAdobephotoshop,
+  Icons.siPostgresql,
+  Icons.siMysql,
+  Icons.siMicrosoftsqlserver,
+  Icons.siMongodb,
+  Icons.siXampp
+];
 
 const IconCloud = () => {
-  return <Cloud options={options}>{icons}</Cloud>;
+  const memoizedIcons = useMemo(() => {
+    return iconsList.map(icon => {
+      return renderSimpleIcon({
+        icon,
+        size: 75
+      });
+    });
+  }, [iconsList]);
+
+  return <Cloud options={options}>{memoizedIcons}</Cloud>;
 };
 
 export default IconCloud;
